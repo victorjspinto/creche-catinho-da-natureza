086ff1b7cb753b713551274339dbe162a6bf6a6e
Victor Pinto <victorjspinto@gmail.com>
2016-05-21 17:07:59 -0300

    Commit inicial do projeto

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crecheApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the crecheApp
+ */
+angular.module('crecheApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
