e929b7216df2ef713b76df9b35f3a60d6a6a285c
Victor J S Pinto <victorjspinto@tecgraf.puc-rio.br>
2017-01-16 20:41:40 -0200

    Inciio do uso do firebase

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,44 @@ declare function require(module: string):any;
 declare var global:any;
 
 // global.$ = global.jQuery = require('jquery');
-import angular = require('angular');
+require('angular');
 require('angular-material');
 require('angular-material/angular-material.css');
 require('./app.css');
+
+var firebase = require('firebase/app');
+var config = {
+  apiKey: "AIzaSyBjAR7takcSRt_3VU7T3grPxA5dUKgfZbo",
+  authDomain: "creche-cantinho-da-natureza.firebaseapp.com",
+  databaseURL: "https://creche-cantinho-da-natureza.firebaseio.com",
+  storageBucket: "creche-cantinho-da-natureza.appspot.com",
+  messagingSenderId: "507255567859"
+};
+firebase.initializeApp(config);
+
+require('angularfire');
 
 import sidebarMenu from "./sidebar-menu/index.ts";
 import app from './app.component.ts';
 import login from './login/login.component.ts';
 
+
 export default angular
-  .module('app', ['ngMaterial', sidebarMenu.name, app.name, login.name]);
+  .module('app', ['ngMaterial', sidebarMenu.name, app.name, login.name, 'firebase'])
+  .run( ($log:ng.ILogService, $firebaseAuth:AngularFireAuth) => {
+    $firebaseAuth.$authWithPassword({ email: "teste@teste", password: "password" })
+      .then((result) => {
+        $log.info(result);
+      });
+    // var db = new Firebase("https://creche-cantinho-da-natureza.firebaseio.com") ;
+    // db.authWithPassword({ email: "teste", password : "teste2"}, (error, success) => {
+    //   if(error) {
+    //     $log.error(error);
+    //   } else {
+    //     $log.info(success);
+    //   }
+    // });
+
+    
+  });
 
