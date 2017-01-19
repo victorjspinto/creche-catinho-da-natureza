8de31d919ecf901b82beffbb34cc6bdd7387f363
Victor J S Pinto <victorjspinto@tecgraf.puc-rio.br>
2017-01-19 20:16:21 -0200

    Dados dos irmao do aluno :)

---
+++ src/app/student/brothers.component.ts
@@ -0,0 +1,47 @@
+class BrothersController {
+
+    public state:String = 'List';
+    private action:String;
+    private selected:any;
+
+    public brothers = [];
+
+    public add() {
+        this.state = 'Edit';
+        this.action = 'New';
+        this.selected = {};
+    }
+
+    public edit(selected) {
+        this.state = 'Edit';
+        this.action = 'Alter';
+        this.selected = selected;
+    }
+
+    public save() {
+        if(this.action == 'New') {
+            this.brothers.push(this.selected);
+            this.selected = {};
+        }
+        this.state = 'List'
+    }
+
+    public cancel() {
+        this.state = 'List';
+        this.selected = {};
+    }
+
+    public remove(item) {
+        let index = this.brothers.indexOf(item);
+        if(index > -1) {
+            this.brothers.splice(index, 1)
+        }
+    }
+}
+
+export default angular.module('app.student.brothers', [])
+    .component('studentBrothers', {
+        template: require('./brothers.html'),
+        controller: BrothersController,
+        controllerAs: 'ctrl'
+    }).name;
--- src/app/student/student.ts
+++ src/app/student/student.ts
@@ -2,5 +2,6 @@ import studentList from './student-list.component'
 import newStudent from './new-student.component'
 import responsible from './responsible.component'
 import parent from './parent.component'
+import brother from './brothers.component'
 
-export default angular.module('student', [studentList, newStudent, responsible, parent]).name
+export default angular.module('student', [studentList, newStudent, responsible, parent, brother]).name
