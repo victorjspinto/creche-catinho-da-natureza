04d53b729951b123d8f546d4f6cfec3951623ec2
Victor Pinto <victorjspinto@gmail.com>
2017-01-26 00:17:48 -0200

    Refactory dos sub-modulos de student para pasta separada

--- src/app/student/student.ts
+++ src/app/student/student.ts
@@ -1,7 +1,7 @@
 import studentList from './student-list.component'
 import newStudent from './new-student.component'
-import responsible from './responsible.component'
-import parent from './parent.component'
-import brother from './brothers.component'
+import responsible from './responsible/responsible.component'
+import parent from './parent/parent.component'
+import brother from './brother/brothers.component'
 
 export default angular.module('student', [studentList, newStudent, responsible, parent, brother]).name
