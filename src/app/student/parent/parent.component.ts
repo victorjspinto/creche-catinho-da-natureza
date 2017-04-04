f63a64dd0ea2e160e9f3a741294efd50121b5b08
Victor J S Pinto <victorjspinto@tecgraf.puc-rio.br>
2017-04-03 21:58:51 -0300

    Formulario pai / mae

--- src/app/student/parent/parent.component.ts
+++ src/app/student/parent/parent.component.ts
@@ -1,11 +1,22 @@
+import { RaceService, Race } from './../../miscelaneous/race.service'
 
 class StudentParentController {
-    
+    public races:Array<Race>;
+
+    constructor(
+        private raceService:RaceService,
+    ) {
+        this.races = raceService.find();
+    }
+
 }
 
 export default angular.module('app.student.parent', [])
     .component('studentParent', {
         template: require('./parent.html'),
         controller: StudentParentController,
-        controllerAs: 'ctrl'
+        controllerAs: 'ctrl',
+        bindings: {
+            parent: "="
+        }
     }).name
